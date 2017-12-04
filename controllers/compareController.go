0427761e94e68464dc136af4b721268c177e5d9d
David Roth <droth@shipt.com>
2017-12-03 22:24:12 -0800

    slow it down

--- controllers/compareController.go
+++ controllers/compareController.go
@@ -18,29 +18,17 @@ func Compare(w http.ResponseWriter, r *http.Request) {
 	vars := r.URL.Query()
 	pickup := strings.ToLower(vars["pickup"][0])
 	dropoff := strings.ToLower(vars["dropoff"][0])
-	pickupChan := make(chan models.LatLng)
-	defer close(pickupChan)
-	dropoffChan := make(chan models.LatLng)
-	defer close(dropoffChan)
-
-	go func() { pickupChan <- google.GetLatLong(pickup) }()
-	go func() { dropoffChan <- google.GetLatLong(dropoff) }()
-
-	pickupLoc := <-pickupChan
-	dropoffLoc := <-dropoffChan
+	pickupLoc := google.GetLatLong(pickup)
+	dropoffLoc := google.GetLatLong(dropoff)
 	pickupLat, pickupLong := pickupLoc.Lat, pickupLoc.Lng
 	dropoffLat, dropoffLong := dropoffLoc.Lat, dropoffLoc.Lng
 
-	c := make(chan models.CompareResponse)
-	defer close(c)
-	go func() { c <- lyft.Estimate(pickupLat, pickupLong, dropoffLat, dropoffLong) }()
-	go func() { c <- uber.Estimate(pickupLat, pickupLong, dropoffLat, dropoffLong) }()
 	estimates := []models.CompareResponse{}
+	lyftEst := lyft.Estimate(pickupLat, pickupLong, dropoffLat, dropoffLong)
+	estimates = append(estimates, lyftEst)
+	uberEst := uber.Estimate(pickupLat, pickupLong, dropoffLat, dropoffLong)
+	estimates = append(estimates, uberEst)
 
-	for i := 0; i < 2; i++ {
-		estimate := <-c
-		estimates = append(estimates, estimate)
-	}
 	var resp models.CompareJSON
 	for _, est := range estimates {
 		resp.Results = append(resp.Results, est)
